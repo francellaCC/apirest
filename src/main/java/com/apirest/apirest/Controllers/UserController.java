1fb770965e59ad4aad42934186a54cd2b2b4d4b8
francellaCC <102675185+francellaCC@users.noreply.github.com>
2025-05-16 14:27:48 -0600

    feat: Creacion de usario

--- src/main/java/com/apirest/apirest/Controllers/UserController.java
+++ src/main/java/com/apirest/apirest/Controllers/UserController.java
@@ -1,17 +1,27 @@
 package com.apirest.apirest.Controllers;
 
+
+import com.apirest.apirest.Entities.User;
+import com.apirest.apirest.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-    @GetMapping
-    public ResponseEntity<String> obtenerTodosLosUsuarios() {
-        return new ResponseEntity<>("¡Obtención de usuarios exitosa!", HttpStatus.OK);
+    @Autowired
+    private UserRepository userRepo;
+//    @GetMapping
+//    public ResponseEntity<String> obtenerTodosLosUsuarios() {
+//        return new ResponseEntity<>("¡Obtención de usuarios exitosa!", HttpStatus.OK);
+//    }
+
+    @PostMapping("/register")
+    public User createUser(@RequestBody User user){
+        System.out.println("User:" + user);
+        return userRepo.save(user);
     }
 }
